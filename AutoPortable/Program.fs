c6757622cc5305c97fa68a68f33bfd21811f4ec7
Paul Staszko <paulstaszko@gmail.com>
2024-07-22 17:50:58 -0500

    ok

---
+++ AutoPortable/Program.fs
@@ -0,0 +1,3 @@
+﻿namespace AutoPortable
+module Main =
+    ()
---
+++ CompileScripts.bat
@@ -0,0 +1,7 @@
+#Op: IndentOn /
+.\Compiler\Ahk2Exe.exe /in "HotSocket_Server.ahk" /base ".\Compiler\Unicode 64-bit.bin"
+.\Compiler\Ahk2Exe.exe /in "MQTT_Pub.ahk"         /base ".\Compiler\Unicode 64-bit.bin"
+.\Compiler\Ahk2Exe.exe /in "MQTT_Sub.ahk"         /base ".\Compiler\Unicode 64-bit.bin"
+.\Compiler\Ahk2Exe.exe /in "PortableRunner.ahk"   /base ".\Compiler\Unicode 64-bit.bin"
+.\Compiler\Ahk2Exe.exe /in "RunVanilla.ahk"       /base ".\Compiler\Unicode 64-bit.bin"
+#Op: End
