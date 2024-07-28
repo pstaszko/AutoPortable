cf82325027ae30c7dde8f7792434de22141d4201
Paul Staszko <paulstaszko@gmail.com>
2024-07-27 21:20:36 -0500

    ok

--- ahkpm-modules/github.com/pstaszko/AHK-Notification/AHK-Notification-FSharp/README.md
+++
@@ -1,3 +0,0 @@
-# AHK-Notification-FSharp
-
-AHK-Notification-FSharp provides a simple way to display toast notifications from dotnet applications.
--- ahkpm-modules/github.com/pstaszko/AHK-Notification/CompileScript.bat
+++ ahkpm-modules/github.com/pstaszko/AHK-Notification/CompileScript.bat
@@ -1,1 +1,1 @@
-.\Compiler\Ahk2Exe.exe /in "AHK-Notification.ahk" /base ".\Compiler\Unicode 64-bit.bin" /out .\AHK-Notification-FSharp\AHK-Notification\
+.\Compiler\Ahk2Exe.exe /in "AHK-Notification.ahk" /base ".\Compiler\Unicode 64-bit.bin"
