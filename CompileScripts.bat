3b5e28db388cc663d636c198bd3e2703a80d32f6
Paul Staszko <paulstaszko@gmail.com>
2024-09-02 18:35:16 -0500

    ok

--- CompileScripts.bat
+++
@@ -1,5 +0,0 @@
-.\ahkpm-modules\github.com\pstaszko\AHK_Compiler\Ahk2Exe.exe /base ".\ahkpm-modules\github.com\pstaszko\AHK_Compiler\v1.1.37.02\Unicode 64-bit.bin" /in "HotSocket_Server.ahk"
-.\ahkpm-modules\github.com\pstaszko\AHK_Compiler\Ahk2Exe.exe /base ".\ahkpm-modules\github.com\pstaszko\AHK_Compiler\v1.1.37.02\Unicode 64-bit.bin" /in "MQTT_Pub.ahk"
-.\ahkpm-modules\github.com\pstaszko\AHK_Compiler\Ahk2Exe.exe /base ".\ahkpm-modules\github.com\pstaszko\AHK_Compiler\v1.1.37.02\Unicode 64-bit.bin" /in "MQTT_Sub.ahk"
-.\ahkpm-modules\github.com\pstaszko\AHK_Compiler\Ahk2Exe.exe /base ".\ahkpm-modules\github.com\pstaszko\AHK_Compiler\v1.1.37.02\Unicode 64-bit.bin" /in "PortableRunner.ahk"
-.\ahkpm-modules\github.com\pstaszko\AHK_Compiler\Ahk2Exe.exe /base ".\ahkpm-modules\github.com\pstaszko\AHK_Compiler\v1.1.37.02\Unicode 64-bit.bin" /in "RunVanilla.ahk"
